cd1c323dc15adcf7b426f98288d2c380ba389171
10xdev4u-alt <10xdev4u@gmail.com>
2026-02-07 00:25:18 +0530

    feat: add KafkaCnsmr

--- src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
+++ src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
@@ -6,9 +6,23 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
-
+    
+    private final DatabaseConduit databaseConduit;
+    private int transactionCount = 0;
+    
+    public KafkaConsumer(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
+    
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core-group")
     public void listen(Transaction transaction) {
-        System.out.println(">>> RECEIVED: Amount = " + transaction.getAmount());
+        transactionCount++;
+        System.out.println(">>> [" + transactionCount + "] RECEIVED: Amount = " + transaction.getAmount());
+        databaseConduit.processTransaction(transaction);
+        
+        // Print all balances every 5 transactions to see progress
+        if (transactionCount % 5 == 0) {
+            databaseConduit.printAllUsers();
+        }
     }
 }
